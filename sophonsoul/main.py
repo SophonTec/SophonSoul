80aeb7c7d504e37100edd8d1eb0a9cc3908a99bf
Sophon Tec <sophontec@gmail.com>
2020-10-18 22:43:44 +0800

    SSO-2: Setup the Virtual Environment (1/1)
    [Problem]
    Setup python virtual environments.
    
    [Implementation]
    Setup the virtual environment of python3.7 and python3.8 via venv
    package.
    
    [Test]
    Success
    
    [Notes]
    
    [Features]
    python3.7, python3.8, venv

---
+++ sophonsoul/main.py
@@ -0,0 +1,20 @@
+import torch
+
+
+
+
+class Brain:
+    
+    def __init__(self):
+        pass
+
+    def sync(self):
+        pass
+
+    def inspect(self):
+        pass
+
+    def get_input(self):
+        pass
+
+    def 
---
+++ tests/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+# test.py
+#
+# Copyright (C) 2020 Sheng Qiang. All rights reserved.
+#
+# PROPRIETARY/CONFIDENTIAL.
+#
+# Use is subject to license terms and restrictions.
+#
+
+import torch
+
+import gym
+
